453fc514cdde7aa7aa59506ec93286bcf529c3f8
amirisback <faisalamircs@gmail.com>
2022-07-15 10:27:53 +0700

    add: flutter module

--- app/src/main/java/com/frogobox/research/MainActivity.kt
+++ app/src/main/java/com/frogobox/research/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.frogobox.research
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startActivity(FlutterActivity.createDefaultIntent(this))
+
     }
 }
